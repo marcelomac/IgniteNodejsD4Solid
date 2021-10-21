aa7e5be5edcbff6542b0bd8046a1d36b8bc30329
Marcelo A. Macagnan <marcelo.a.mac@gmail.com>
2021-10-21 11:47:45 -0300

    2021-10-21

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,11 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const user_id = request.headers.user_id as string;
+
+    const list = this.listAllUsersUseCase.execute({ user_id });
+
+    return response.status(200).json(list);
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,12 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    const user = this.turnUserAdminUseCase.execute({ user_id });
+
+    // return response.status(200).send(`User ${user.name} is admin`);
+    return response.status(200).json(user);
   }
 }
 
